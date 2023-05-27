6826431a6d2fab64496fd2ba26b24c8af44b0dee
mpw <x@mpw.sh>
2023-05-26 23:25:02 -0300

    use string for build command instead

--- crates/lib/src/crd.rs
+++ crates/lib/src/crd.rs
@@ -15,7 +15,6 @@ use crate::prelude::*;
     derive = "PartialEq",
     namespaced
 )]
-
 #[serde(rename_all = "camelCase")]
 pub struct TailoredAppSpec {
     pub labels: BTreeMap<String, String>,
@@ -31,9 +30,9 @@ pub struct Container {
     pub image: String,
     pub port: i32,
     #[serde(skip_serializing_if = "Option::is_none")]
-    pub build_command: Option<Vec<String>>,
+    pub build_command: Option<String>,
     #[serde(skip_serializing_if = "Option::is_none")]
-    pub run_command: Option<Vec<String>>,
+    pub run_command: Option<String>,
     #[serde(skip_serializing_if = "Option::is_none")]
     pub git_repository: Option<String>,
     #[serde(skip_serializing_if = "Option::is_none")]
--- crates/lib/src/prelude.rs
+++ crates/lib/src/prelude.rs
@@ -1,6 +1,6 @@
 pub use std::collections::BTreeMap;
 
 pub use kube::Resource;
-pub use serde::{Deserialize, Serialize};
+pub use serde::{Deserialize, Serialize, Serializer};
 
 pub use crate::crd::{Container, Deployment, Domains, Ingress, TailoredApp, TailoredAppSpec};
