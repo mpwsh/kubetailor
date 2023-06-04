30f61a81100d4371a95ddf9d989a63ef136c6352
mpw <x@mpw.sh>
2023-06-03 23:44:38 -0300

    skip serializing fields if null

--- crates/server/src/tapp.rs
+++ crates/server/src/tapp.rs
@@ -9,11 +9,14 @@ use super::{config::Kubetailor, deployment::Deployment, error::TappRequestError}
 #[derive(Serialize, Deserialize, Debug)]
 pub struct TappRequest {
     pub name: String,
+    #[serde(skip_serializing)]
     pub owner: String,
     pub group: String,
     pub container: Container,
     pub domains: Domains,
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub env_vars: Option<BTreeMap<String, String>>,
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub secrets: Option<BTreeMap<String, String>>,
     #[serde(skip_deserializing, skip_serializing)]
     pub kubetailor: Kubetailor,
